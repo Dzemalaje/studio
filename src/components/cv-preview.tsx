380e0bede1be7b1f08164795cf46b265a283430f
Dzemal Humic <DzemalHumic@gmail.com>
2025-08-02 21:53:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/cv-preview.tsx
+++ src/components/cv-preview.tsx
@@ -54,3 +54,4 @@ export const CVPreview = memo(function CVPreview() {
         </div>
     </div>
   );
+});
--- src/components/image-cropper.tsx
+++ src/components/image-cropper.tsx
@@ -145,3 +145,4 @@ export const ImageCropper = memo(function ImageCropper({ imageSrc, onCropComplet
       )}
     </div>
   );
+});
